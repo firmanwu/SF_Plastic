6b4f93ea34b0091aee6c05fd9276269825db66b9
Fernando Hernandez <fernando.hernandez@seberon.com>
2018-09-04 00:12:46 +0800

    Language update to Simplified Chinese as default one. It is just a reference for future translation to Traditional Chinese
    README_LANG file with some tips

---
+++ application/logs/log-2018-09-04.php
@@ -0,0 +1,60 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-09-04 00:08:33 --> Config Class Initialized
+INFO - 2018-09-04 00:08:33 --> Hooks Class Initialized
+DEBUG - 2018-09-04 00:08:33 --> UTF-8 Support Enabled
+INFO - 2018-09-04 00:08:33 --> Utf8 Class Initialized
+INFO - 2018-09-04 00:08:33 --> URI Class Initialized
+INFO - 2018-09-04 00:08:33 --> Router Class Initialized
+INFO - 2018-09-04 00:08:33 --> Output Class Initialized
+INFO - 2018-09-04 00:08:33 --> Security Class Initialized
+DEBUG - 2018-09-04 00:08:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-09-04 00:08:33 --> Input Class Initialized
+INFO - 2018-09-04 00:08:33 --> Language Class Initialized
+INFO - 2018-09-04 00:08:33 --> Loader Class Initialized
+INFO - 2018-09-04 00:08:33 --> Helper loaded: url_helper
+INFO - 2018-09-04 00:08:33 --> Controller Class Initialized
+INFO - 2018-09-04 00:08:33 --> File loaded: /var/www/html/sf_plastic/application/views/bootstrap/home3.php
+INFO - 2018-09-04 00:08:33 --> Final output sent to browser
+DEBUG - 2018-09-04 00:08:33 --> Total execution time: 0.0373
+INFO - 2018-09-04 00:08:38 --> Config Class Initialized
+INFO - 2018-09-04 00:08:38 --> Hooks Class Initialized
+DEBUG - 2018-09-04 00:08:38 --> UTF-8 Support Enabled
+INFO - 2018-09-04 00:08:38 --> Utf8 Class Initialized
+INFO - 2018-09-04 00:08:38 --> URI Class Initialized
+INFO - 2018-09-04 00:08:38 --> Router Class Initialized
+INFO - 2018-09-04 00:08:38 --> Output Class Initialized
+INFO - 2018-09-04 00:08:38 --> Security Class Initialized
+DEBUG - 2018-09-04 00:08:38 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-09-04 00:08:38 --> Input Class Initialized
+INFO - 2018-09-04 00:08:38 --> Language Class Initialized
+INFO - 2018-09-04 00:08:38 --> Loader Class Initialized
+INFO - 2018-09-04 00:08:38 --> Helper loaded: url_helper
+INFO - 2018-09-04 00:08:38 --> Controller Class Initialized
+INFO - 2018-09-04 00:08:38 --> Database Driver Class Initialized
+INFO - 2018-09-04 00:08:38 --> Helper loaded: form_helper
+INFO - 2018-09-04 00:08:38 --> Form Validation Class Initialized
+DEBUG - 2018-09-04 00:08:38 --> Config file loaded: /var/www/html/sf_plastic/application/config/grocery_crud.php
+INFO - 2018-09-04 00:08:38 --> Model "Grocery_crud_model" initialized
+INFO - 2018-09-04 00:08:39 --> File loaded: /var/www/html/sf_plastic/application/views/materials_formulas.php
+INFO - 2018-09-04 00:08:39 --> Final output sent to browser
+DEBUG - 2018-09-04 00:08:39 --> Total execution time: 0.2808
+INFO - 2018-09-04 00:08:39 --> Config Class Initialized
+INFO - 2018-09-04 00:08:39 --> Hooks Class Initialized
+DEBUG - 2018-09-04 00:08:39 --> UTF-8 Support Enabled
+INFO - 2018-09-04 00:08:39 --> Utf8 Class Initialized
+INFO - 2018-09-04 00:08:39 --> URI Class Initialized
+INFO - 2018-09-04 00:08:39 --> Router Class Initialized
+INFO - 2018-09-04 00:08:39 --> Output Class Initialized
+INFO - 2018-09-04 00:08:39 --> Security Class Initialized
+DEBUG - 2018-09-04 00:08:39 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-09-04 00:08:39 --> Input Class Initialized
+INFO - 2018-09-04 00:08:39 --> Language Class Initialized
+INFO - 2018-09-04 00:08:39 --> Loader Class Initialized
+INFO - 2018-09-04 00:08:39 --> Helper loaded: url_helper
+INFO - 2018-09-04 00:08:39 --> Controller Class Initialized
+INFO - 2018-09-04 00:08:39 --> Database Driver Class Initialized
+INFO - 2018-09-04 00:08:39 --> Helper loaded: form_helper
+INFO - 2018-09-04 00:08:39 --> Form Validation Class Initialized
+DEBUG - 2018-09-04 00:08:39 --> Config file loaded: /var/www/html/sf_plastic/application/config/grocery_crud.php
+INFO - 2018-09-04 00:08:39 --> Model "Grocery_crud_model" initialized
