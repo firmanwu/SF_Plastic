f98f2ad28b665def82678a3d7f5dcea63b1285f6
William Wu <william.wu@seberon.com>
2018-09-12 09:32:12 +0800

    Revise standard Chinese translation.

--- assets/grocery_crud/languages/chinese.php
+++ assets/grocery_crud/languages/chinese.php
@@ -2,101 +2,100 @@
 /* Translated by @heenji */
 	$lang['list_add'] 				= '新增';
 	$lang['list_actions'] 			= '操作';
-	$lang['list_page'] 				= '页';
+	$lang['list_page'] 				= '頁';
 	$lang['list_paging_of'] 		= '/';
-	$lang['list_displaying']		= '显示 {start} 至 {end} /总计 {results} 记录';
-	$lang['list_filtered_from']		= '(筛选于 {total_results} 总记录)';
-	$lang['list_show_entries']		= '显示 {paging} 页记录';
-	$lang['list_no_items']			= '没有记录';
-	$lang['list_zero_entries']		= '显示 0 - 0 / 0 条记录';
-	$lang['list_search'] 			= '查询';
-	$lang['list_search_all'] 		= '查询所有';
-	$lang['list_clear_filtering'] 	= '删除筛选条件';
-	$lang['list_delete'] 			= '删除';
-	$lang['list_edit'] 				= '编辑';
-	$lang['list_paging_first'] 		= '首页';
-	$lang['list_paging_previous'] 	= '前一页';
-	$lang['list_paging_next'] 		= '后一页';
-	$lang['list_paging_last'] 		= '最后';
-	$lang['list_loading'] 			= '载入...';
+	$lang['list_displaying']		= '顯示 {start} 到 {end} 筆紀錄 / 總計 {results} 筆紀錄';
+	$lang['list_filtered_from']		= '(從 {total_results} 筆紀錄中篩選)';
+	$lang['list_show_entries']		= '顯示 {paging} 筆紀錄';
+	$lang['list_no_items']			= '無紀錄';
+	$lang['list_zero_entries']		= '顯示 0 筆紀錄';
+	$lang['list_search'] 			= '查詢';
+	$lang['list_search_all'] 		= '查詢全部';
+	$lang['list_clear_filtering'] 	= '刪除篩選條件';
+	$lang['list_delete'] 			= '刪除';
+	$lang['list_edit'] 				= '編輯';
+	$lang['list_paging_first'] 		= '首頁';
+	$lang['list_paging_previous'] 	= '前一頁';
+	$lang['list_paging_next'] 		= '後一頁';
+	$lang['list_paging_last'] 		= '最後';
+	$lang['list_loading'] 			= '載入中...';
 
-	$lang['form_edit'] 				= '编辑';
+	$lang['form_edit'] 				= '編輯';
 	$lang['form_back_to_list'] 		= '返回列表';
 	$lang['form_update_changes'] 	= '修改';
 	$lang['form_cancel'] 			= '取消';
-	$lang['form_update_loading'] 	= '载入, 并更新修改...';
+	$lang['form_update_loading'] 	= '載入並更新修改...';
 	$lang['update_success_message'] = '修改成功！';
 	$lang['form_go_back_to_list'] 	= '返回列表';
 
 	$lang['form_add'] 				= '新增';
-	$lang['insert_success_message'] = '新增记录成功保存到数据库';
-	$lang['form_or']				= '或者';
-	$lang['form_save'] 				= '保存';
-	$lang['form_insert_loading'] 	= '载入,并保存...';
+	$lang['insert_success_message'] = '新增紀錄成功!';
+	$lang['form_or']				= '或';
+	$lang['form_save'] 				= '儲存';
+	$lang['form_insert_loading'] 	= '新增並載入...';
 
-	$lang['form_upload_a_file'] 	= '上传文件';
-	$lang['form_upload_delete'] 	= '删除';
-	$lang['form_button_clear'] 		= '清空';
+	$lang['form_upload_a_file'] 	= '上傳檔案';
+	$lang['form_upload_delete'] 	= '刪除';
+	$lang['form_button_clear'] 		= '清除';
 
-	$lang['delete_success_message'] = '从数据库里成功删除你的数据.';
-	$lang['delete_error_message'] 	= '没有从数据库中删除你的数据.';
+	$lang['delete_success_message'] = '刪除紀錄成功!';
+	$lang['delete_error_message'] 	= '刪除紀錄錯誤!';
 
 	/* Javascript messages */
-	$lang['alert_add_form']			= '你新增的数据可能没有保存.\\n确定返回列表吗?';
-	$lang['alert_edit_form']		= '你更新的数据可能没有保存.\\n确定返回列表吗??';
-	$lang['alert_delete']			= '你确定要删除这些记录?';
+	$lang['alert_add_form']			= '新增的紀錄可能沒有儲存\\n確定返回列表嗎?';
+	$lang['alert_edit_form']		= '更新的紀錄可能沒有儲存\\n確定返回列表嗎?';
+	$lang['alert_delete']			= '確定刪除紀錄?';
 
-	$lang['insert_error']			= '新增记录的时候发生错误.';
-	$lang['update_error']			= '保存记录的时候发生错误.';
+	$lang['insert_error']			= '新增紀錄時發生錯誤!';
+	$lang['update_error']			= '更新紀錄時發生錯誤!';
 
 /* Additional translations by @iradave based upon inputs from C. Zhu 7/7/16 */
 	/* Added in version 1.2.1 */
-	$lang['set_relation_title']		= '选择 {field_display_as}';
-	$lang['list_record']			= '记录';
-	$lang['form_inactive']			= '注销';
-	$lang['form_active']			= '激活';
+	$lang['set_relation_title']		= '選擇 {field_display_as}';
+	$lang['list_record']			= '紀錄';
+	$lang['form_inactive']			= '停用';
+	$lang['form_active']			= '啟用';
 
 	/* Added in version 1.2.2 */
-	$lang['form_save_and_go_back']	= '保存并返回列表';
-	$lang['form_update_and_go_back']= '保存并返回列表';
+	$lang['form_save_and_go_back']	= '儲存並返回列表';
+	$lang['form_update_and_go_back']= '更新並返回列表';
 
 	/* Upload functionality */
-	$lang['string_delete_file'] 	= "正在删除数据";
-	$lang['string_progress'] 		= "进程: ";
-	$lang['error_on_uploading'] 	= "加载时发生了错误.";
-	$lang['message_prompt_delete_file'] 	= "确定删除这些文件?";
+	$lang['string_delete_file'] 	= '正在刪除檔案';
+	$lang['string_progress'] 		= '進度: ';
+	$lang['error_on_uploading'] 	= '上傳時發生錯誤!';
+	$lang['message_prompt_delete_file'] 	= '確定刪除檔案?';
 
-	$lang['error_max_number_of_files'] 	= "一次只能上传一份文件.";
-	$lang['error_accept_file_types'] 	= "不允许上传这种类型的拓展文件.";
-	$lang['error_max_file_size'] 		= "上传的文件超过了指示所规定的最大尺寸.";
-	$lang['error_min_file_size'] 		= "不能上传空的文件.";
+	$lang['error_max_number_of_files'] 	= '一次只能上傳一個檔案';
+	$lang['error_accept_file_types'] 	= '不接受此上傳檔案的類型';
+	$lang['error_max_file_size'] 		= '上傳的檔案大小過大';
+	$lang['error_min_file_size'] 		= '不能上傳大小為零的檔案';
 
 	/* Added in version 1.3.1 */
-	$lang['list_export'] 	= "导出";
-	$lang['list_print'] 	= "打印";
-	$lang['minimize_maximize'] = '最小值/最大值';
+	$lang['list_export'] 	= '輸出';
+	$lang['list_print'] 	= '列印';
+	$lang['minimize_maximize'] = '最小值 / 最大值';
 	
 	/* Added in version 1.4 */
-	$lang['list_view'] = '浏览';
+	$lang['list_view'] = '瀏覽';
 	
 	/* Added in version 1.5.1 */
-	$lang['ui_day'] = '日期';
-	$lang['ui_month'] = '月份';
-	$lang['ui_year'] = '年份';
+	$lang['ui_day'] = '日';
+	$lang['ui_month'] = '月';
+	$lang['ui_year'] = '年';
 	
 	/* Added in version 1.5.2 */
 	$lang['list_more'] = '更多';
 
 	/* Added in version 1.5.6 */
-	$lang['list_search_column'] = '查询 {column_name}';
+	$lang['list_search_column'] = '搜尋 {column_name}';
 
 	/* Added in version 1.5.8 */
-	$lang['alert_delete_multiple'] = '你确定要删除这些项吗（项_总数: {items_amount}) ?';
+	$lang['alert_delete_multiple'] = '確定要刪除這些紀錄嗎? (總計: {items_amount} 筆紀錄)';
 
-	$lang['alert_delete_multiple_one'] = '你确定要删除此一项吗?';
+	$lang['alert_delete_multiple_one'] = '確定要刪除此紀錄嗎?';
 
 
 
 	/* Added in version 1.6.1 */
-	$lang['list_clone'] = 'Clone';
-
+	$lang['list_clone'] = '複製';
