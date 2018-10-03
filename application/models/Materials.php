37f6f9ae2cf849f8261124e270950b53ba1c4e6c
William Wu <william.wu@seberon.com>
2018-10-03 11:32:09 +0800

    Fix databse not consistent issue.

--- application/models/Materials.php
+++ application/models/Materials.php
@@ -5,7 +5,7 @@ class Materials extends CI_Model {
 	public function get_formula_materials_sorted($formula_id)
     {
             $this->load->database();
-            $this->db->select('material.label, material.material_id, formula_material.amount, formula_material.sort');
+            $this->db->select('material.label, material.material_id, formula_material.weight, formula_material.order');
 			$this->db->from('formula_material');
 			$this->db->join('material', 'formula_material.material_id = material.material_id', 'left');
 			$this->db->where('formula_material.formula_id', $formula_id);
