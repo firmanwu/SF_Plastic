831ad9228f518e3c65b3cc0ec1922a0b42b2fece
William Wu <william.wu@seberon.com>
2018-10-03 17:39:14 +0800

    1. Re-organize the columns of daily_orders_formulas 2. Add required_fields 3. Add callback_add_field for pre-filling the order serial number 4. Add callback_after_insert to increase or generate the serial number

--- application/controllers/Daily_orders_formulas.php
+++ application/controllers/Daily_orders_formulas.php
@@ -17,33 +17,102 @@ class Daily_orders_formulas extends CI_Controller {
 		$this->load->view('daily_orders_formulas.php',(array)$output);
 	}
 
-	public function daily_orders_formulas_management()
-	{
-		try{
-                        $crud = new grocery_CRUD();
+    public function getSerialNumber()
+    {
+        $this->load->helper('file');
+        $this->load->helper('date');
 
-                        $crud->set_theme('bootstrap-v4');
-						$crud->set_table('formula_daily_order');
-						$crud->set_subject('生產排程');
-						//$crud->unset_fields('unit_id');
-						$crud->set_relation('order_id', "daily_order",'order_id');
-						$crud->set_relation('formula_id', "formula",'name');
-						$crud->add_action('Process','','check_materials','el el-qrcode');
-						//$crud->add_action('Process','','','el el-qrcode', array($this, 'prepare_materials'));
-                        $crud->display_as('formula_id', '配方名稱');
-                        $crud->display_as('order_id', '排程單編號');
-                        $crud->display_as('processed', '進度');
-                        $crud->display_as('date', '日期');
-                        $crud->display_as('number', '爐數');
-						//$crud->set_relation('ingredient_id', "ingredient",'label');
+        $dateString = '%Y%m%d';
+        $time = time();
+        $currentDate = mdate($dateString, $time);
+        $fileName = 'OrderSN';
+        if (TRUE == file_exists($fileName)) {
+            $currentSerialNumber = read_file($fileName);
+            if (FALSE == strstr($currentSerialNumber, $currentDate)) {
+                $newSerialNumber = $currentDate . "001";
+                write_file($fileName, $newSerialNumber);
 
-						$output = $crud->render();
-						$this->_example_output($output);
+                return $newSerialNumber;
+            }
+            else {
+                return $currentSerialNumber;
+            }
+        }
+        else {
+            $newSerialNumber = $currentDate . "001";
+            write_file($fileName, $newSerialNumber);
 
-                }catch(Exception $e){
-                        show_error($e->getMessage().' --- '.$e->getTraceAsString());
+            return $newSerialNumber;
+        }
+    }
+
+    public function increaseSerialNumber()
+    {
+        $this->load->helper('file');
+        $this->load->helper('date');
+
+        $dateString = '%Y%m%d';
+        $time = time();
+        $currentDate = mdate($dateString, $time);
+        $fileName = 'OrderSN';
+        if (TRUE == file_exists($fileName)) {
+            $currentSerialNumber = read_file($fileName);
+            if (FALSE == strstr($currentSerialNumber, $currentDate)) {
+                $newSerialNumber = $currentDate . "001";
+            }
+            else {
+                $number = str_replace($currentDate, '', $currentSerialNumber);
+                $number = (int)$number + 1;
+                $length = strlen($number);
+                if (3 >= $length) {
+                    for($i = 0; $i < (3 - $length); $i++)
+                    {
+                        $number = '0' . $number;
+                    }
                 }
-	}
+                else {
+                    $number = '0' . $number;
+                }
+                $newSerialNumber = $currentDate . $number;
+            }
+            write_file($fileName, $newSerialNumber);
+        }
+        else {
+            $newSerialNumber = $currentDate . "001";
+            write_file($fileName, $newSerialNumber);
+        }
+    }
+
+    public function daily_orders_formulas_management()
+    {
+        try{
+                $crud = new grocery_CRUD();
+
+                $crud->set_theme('bootstrap-v4');
+                $crud->set_table('formula_daily_order');
+                $crud->set_subject('生產排程');
+                $crud->set_relation('formula_id', "formula",'name');
+                $crud->add_action('Process','','check_materials','el el-qrcode');
+                $crud->display_as('order_id', '排程單編號');
+                $crud->display_as('formula_id', '配方名稱');
+                $crud->display_as('materialCheck', '備料確認');
+                $crud->display_as('date', '預定生產日期');
+                $crud->display_as('producedAmount', '爐數');
+                $crud->required_fields('order_id', 'formula_id', 'date', 'producedAmount');
+
+                $crud->callback_add_field('order_id', function() {
+                    $datetime = $this->getSerialNumber();
+                    return '<input id="field-order_id" class="form-control" name="order_id" type="text" value=' . $datetime . ' maxlength="255" readonly="yes" />';
+                });
+                $crud->callback_after_insert(array($this, 'increaseSerialNumber'));
+
+                $output = $crud->render();
+                $this->_example_output($output);
+
+        } catch(Exception $e){
+                show_error($e->getMessage().' --- '.$e->getTraceAsString());
+        }
+    }
 
 	public function index()
 	{
